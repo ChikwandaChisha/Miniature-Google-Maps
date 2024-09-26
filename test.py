0aaf0d28db143a5af6d18a485ae7ecf53a48191f
chikwanda <chikwanda.chisha.26@dartmouth.edu>
2024-09-26 18:57:10 -0400

    added test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+from load_graph import load_graph
+
+vertex_dict = load_graph("dartmouth_graph")
+
+out_file = open("vertices.txt", "w")
+for vertex in vertex_dict:
+    out_file.write(str(vertex_dict[vertex]) + "\n")
+out_file.close()
